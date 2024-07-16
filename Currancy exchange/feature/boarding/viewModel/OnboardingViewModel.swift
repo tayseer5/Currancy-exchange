a3cecf0a57d9e391d836cc58ddf4e0f4ed2e5829
TayseerAnwar <tayseer.anwar@linkdev.com>
2024-07-16 15:14:21 +0300

    feat onboarding
    clean up viewmodel

--- Currancy exchange/feature/boarding/viewModel/OnboardingViewModel.swift
+++ Currancy exchange/feature/boarding/viewModel/OnboardingViewModel.swift
@@ -18,24 +18,5 @@ class OnboardingViewModel: ObservableObject {
     @Published var currentIndex: Int = 0
     
     init() {
-           // fetchSlides()
-        }
-
-//    func getCurrentSlider() ->  OnboardingModel{
-//        return slides[currentIndex]
-//    }
-//    
-//    func getSliderCount() ->  Int{
-//        return slides.count
-//    }
-    
-  /* private func fetchSlides() {
-        // Simulate fetching data
-         slides = [
-            OnboardingModel(title: "Take hold of your finances", infoTitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget mauris massa pharetra.", imageName: "onboarding"),
-            OnboardingModel(title: "Smart trading tools", infoTitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget mauris massa pharetra.", imageName: "onboarding2"),
-            OnboardingModel(title: "Invest in the future", infoTitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget mauris massa pharetra.", imageName: "onboarding3")
-        ]
     }
-    */
 }
